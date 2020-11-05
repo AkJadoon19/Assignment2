d008ef3797c2355c836436175560e949ad80a831
AkJadoon19 <73095121+AkJadoon19@users.noreply.github.com>
2020-11-05 02:37:23 -0800

    Update App.js

--- first/src/App.js
+++ first/src/App.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>My name is abdullah</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+
+          Edit <code>src/App.js</code> and save to reload 2017023 .
+=======
+          Edit <code>devops</code> and terminate it 2017023.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
